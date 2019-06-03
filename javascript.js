a1fd8a62d345a3c0c35ada2383c466f531cfdff6
minami <tantatatanaya@yahoo.co.jp>
2019-06-04 07:26:23 +0900

    test

--- javascript.js
+++ javascript.js
@@ -61,12 +61,10 @@ $.ajax({
 const showDialog = () => {
   document.getElementById("dialog").classList.add("show")
   const scrollY = document.documentElement.style.getPropertyValue("--scroll-y");
-  const body = document.body;
   body.style.position = "fixed";
   body.style.top = "-${scrollY}";
 };
 const closeDialog = () => {
-  const body = document.body;
   const scrollY = body.style.top;
   body.style.position = "";
   body.style.top = "";
