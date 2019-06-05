77ba9247f051b0d50dd6ddf799ac88e6112eb58e
minami <tantatatanaya@yahoo.co.jp>
2019-06-05 12:37:02 +0900

    add loader

--- javascript.js
+++ javascript.js
@@ -72,6 +72,18 @@ $.ajax({
         state = false;
       }
     });
+// ああ
 
-
+	
+$('head').append(
+    '<style type="text/css">#contents { display: none; } #fade, #loader { display: block; }</style>'
+);
+ 
+jQuery.event.add(window,"load",function() { // 全ての読み込み完了後に呼ばれる関数
+    var pageH = $("#contents").height();
+ 
+    $("#fade").css("height", pageH).delay(900).fadeOut(800);
+    $("#loader").delay(600).fadeOut(300);
+    $("#contents").css("display", "block");
+});
 
